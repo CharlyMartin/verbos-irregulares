3e420096b4dc27385eb62847c3bd8e674b565e90
Charly Martin <charly.martin.1207@gmail.com>
2016-11-17 20:42:59 +0100

    new page for method

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,7 +1,10 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
+  skip_before_action :authenticate_user!, only: [ :home, :method ]
 
   def home
     @verbs = Verb.all
+  end
+
+  def method
   end
 end
