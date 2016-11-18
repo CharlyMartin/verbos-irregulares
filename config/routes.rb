9cafff6ed7a2d224958a66717e551c12e954db65
Charly Martin <charly.martin.1207@gmail.com>
2016-11-18 12:10:23 +0100

    I18n set up, before testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'pages#home'
-  get '/method', to: 'pages#method', as: 'method'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  scope '(:locale)', locale: /en/ do
+    root to: 'pages#home'
+    get '/method', to: 'pages#method', as: 'method'
+  end
+
 end
